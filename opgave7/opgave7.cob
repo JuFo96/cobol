91a4d7c4924c996fda9cdb80def6e9bcc2a2e336
Julius <julius.foverskov@specialisterne.com>
2025-11-12 10:41:23 +0100

    opgave 7 working, do never ever use øæå with cobol

--- opgave7/opgave7.cob
+++ opgave7/opgave7.cob
@@ -37,16 +37,10 @@
        working-storage section.
            01 end-of-file PIC X value "N".
            01 full-name PIC X(40).
-           01 addresse PIC X.
-
-           01 ws-street PIC X(30).
-           01 ws-house PIC X(5).
-           01 ws-etage PIC X(5).
-           01 ws-side PIC X(5).
+           01 addresse PIC X(100).
 
 
        procedure division.
-       main-procedure.
                open input input-file
                open output output-file
 
@@ -55,45 +49,83 @@
                at end
                    move "Y" to end-of-file
                not at end
-      * ID to field             
-                   move customer-id to navn-adr
-                   write output-record
-      * Name to field
-                   STRING 
-                       first-name                        
-                           delimited by space
-                       " "
-                           delimited by size 
-                       last-name 
-                           delimited by size
-                       into full-name
-                   move full-name to navn-adr
-                   write output-record
-      * addres to field
-
-           MOVE FUNCTION TRIM(street-name) TO ws-street
-           MOVE FUNCTION TRIM(house-number) TO ws-house
-           MOVE FUNCTION TRIM(etage) TO ws-etage
-           MOVE FUNCTION TRIM(side) TO ws-side
-
-           STRING 
-               ws-street DELIMITED BY space
-               " " DELIMITED BY space
-               ws-house DELIMITED BY space
-               " " DELIMITED BY space
-               ws-etage DELIMITED BY space
-               " " DELIMITED BY space
-               ws-side DELIMITED BY space
-               INTO addresse
-           END-STRING
-               move addresse to navn-adr
-                   write output-record
-                   
-                   
-                   display "kunde id: " ws-house ws-street
-                   end-read
+               perform handle-customer
+               move "--------------------------------------" to navn-adr
+               write output-record
+           
+               end-read
                end-perform
                close input-file
                close output-file
-       stop run.
+               stop run.
+           
+           handle-customer.
+             perform format-id
+             perform format-navn
+             perform format-addresse
+             perform format-by
+             perform format-kontakt
+           exit.
+         
+
+           format-id.
+               move spaces to navn-adr
+               string             
+                   "ID: " 
+                   function trim(customer-id)
+                   into navn-adr
+               end-string
+                   write output-record
+           exit.
+
+           format-navn.
+               move spaces to navn-adr
+               STRING 
+                   "Navn: "
+                   function trim(first-name) " "                        
+                   function trim(last-name) 
+                   into navn-adr
+               end-string
+                   write output-record
+           exit.
+
+
+
+
+           format-addresse.
+           move spaces to navn-adr
+           string 
+               "Addresse: "
+               function TRIM(street-name) " "
+               function TRIM(house-number) ", "
+               function TRIM(etage) "."
+               function TRIM(side)  
+               into navn-adr
+           end-string
+               write output-record
+           exit.
+            
+           format-by.
+           move spaces to navn-adr
+           display postnr
+           string 
+              "By: "
+              function TRIM(postnr) " "
+              function TRIM(bynavn) " "
+              into navn-adr
+           end-string
+               write output-record  
+           exit.            
+           
+           format-kontakt.
+           move spaces to navn-adr
+           string 
+              "Kontakt: " "tlf:" 
+              function TRIM(telefon) " email:"
+              function TRIM(email)
+              into navn-adr
+           end-string
+               write output-record
+           exit.
+       
        
