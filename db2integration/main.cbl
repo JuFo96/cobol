186c271fc2a637be0e73d12c40943b5ef54fbc60
Julius <julius.foverskov@specialisterne.com>
2025-11-14 14:26:59 +0100

    add precompiled files, doesn't work missing libraries

---
+++ db2integration/main.cbl
@@ -0,0 +1,156 @@
+       identification division.
+       program-id. upload-db2.
+
+       environment division.
+       
+
+       data division.
+       working-storage section.
+
+       01  SQLDA-ID pic 9(4) comp-5.
+       01  SQLDSIZE pic 9(4) comp-5.
+       01  SQL-STMT-ID pic 9(4) comp-5.
+       01  SQLVAR-INDEX pic 9(4) comp-5.
+       01  SQL-DATA-TYPE pic 9(4) comp-5.
+       01  SQL-HOST-VAR-LENGTH pic 9(9) comp-5.
+       01  SQL-S-HOST-VAR-LENGTH pic 9(9) comp-5.
+       01  SQL-S-LITERAL pic X(258).
+       01  SQL-LITERAL1 pic X(130).
+       01  SQL-LITERAL2 pic X(130).
+       01  SQL-LITERAL3 pic X(130).
+       01  SQL-LITERAL4 pic X(130).
+       01  SQL-LITERAL5 pic X(130).
+       01  SQL-LITERAL6 pic X(130).
+       01  SQL-LITERAL7 pic X(130).
+       01  SQL-LITERAL8 pic X(130).
+       01  SQL-LITERAL9 pic X(130).
+       01  SQL-LITERAL10 pic X(130).
+       01  SQL-IS-LITERAL pic 9(4) comp-5 value 1.
+       01  SQL-IS-INPUT-HVAR pic 9(4) comp-5 value 2.
+       01  SQL-CALL-TYPE pic 9(4) comp-5.
+       01  SQL-SECTIONUMBER pic 9(4) comp-5.
+       01  SQL-INPUT-SQLDA-ID pic 9(4) comp-5.
+       01  SQL-OUTPUT-SQLDA-ID pic 9(4) comp-5.
+       01  SQL-VERSION-NUMBER pic 9(4) comp-5.
+       01  SQL-ARRAY-SIZE pic 9(4) comp-5.
+       01  SQL-IS-STRUCT  pic 9(4) comp-5.
+       01  SQL-IS-IND-STRUCT pic 9(4) comp-5.
+       01  SQL-STRUCT-SIZE pic 9(4) comp-5.
+       01  SQLA-PROGRAM-ID.
+           05 SQL-PART1 pic 9(4) COMP-5 value 172.
+           05 SQL-PART2 pic X(6) value "AEAWAI".
+           05 SQL-PART3 pic X(24) value "LBH2MOLp01111 2         ".
+           05 SQL-PART4 pic 9(4) COMP-5 value 8.
+           05 SQL-PART5 pic X(8) value "DB2INST1".
+           05 SQL-PART6 pic X(120) value LOW-VALUES.
+           05 SQL-PART7 pic 9(4) COMP-5 value 8.
+           05 SQL-PART8 pic X(8) value "MAIN    ".
+           05 SQL-PART9 pic X(120) value LOW-VALUES.
+                               
+       
+      *EXEC SQL INCLUDE SQLCA END-EXEC
+      * SQL Communication Area - SQLCA
+       COPY 'sqlca.cbl'.
+
+                                       
+	
+      *EXEC SQL BEGIN DECLARE SECTION END-EXEC.
+       01 DB-USER PIC X(8) USAGE DISPLAY VALUE "db2inst1".
+       01 DB-PASSWORD PIC x(8) USAGE DISPLAY VALUE "password".
+       
+      *EXEC SQL END DECLARE SECTION END-EXEC
+                                             
+       
+       01  var-data pic x(20) USAGE DISPLAY value "hello world".
+       procedure division.
+          display var-data.
+	
+      *EXEC SQL 
+      *CONNECT TO testdb
+      *	USER :DB-USER
+      *	USING :DB-PASSWORD
+      *	END-EXEC
+           CALL "sqlgstrt" USING
+              BY CONTENT SQLA-PROGRAM-ID
+              BY VALUE 0
+              BY REFERENCE SQLCA
+           CALL "sqlgmf" USING
+              BY VALUE 0
+
+           MOVE 1 TO SQL-STMT-ID 
+           MOVE 3 TO SQLDSIZE 
+           MOVE 2 TO SQLDA-ID 
+
+           CALL "sqlgaloc" USING
+               BY VALUE SQLDA-ID 
+                        SQLDSIZE
+                        SQL-STMT-ID
+                        0
+
+           MOVE "testdb"
+            TO SQL-LITERAL1
+           MOVE 6 TO SQL-HOST-VAR-LENGTH
+           MOVE 452 TO SQL-DATA-TYPE
+           MOVE 0 TO SQLVAR-INDEX
+           MOVE 2 TO SQLDA-ID
+
+           CALL "sqlgstlv" USING 
+            BY VALUE SQLDA-ID
+                     SQLVAR-INDEX
+                     SQL-DATA-TYPE
+                     SQL-HOST-VAR-LENGTH
+            BY REFERENCE SQL-LITERAL1
+            BY VALUE 0
+                     0
+
+           MOVE 8 TO SQL-HOST-VAR-LENGTH
+           MOVE 452 TO SQL-DATA-TYPE
+           MOVE 1 TO SQLVAR-INDEX
+           MOVE 2 TO SQLDA-ID
+
+           CALL "sqlgstlv" USING 
+            BY VALUE SQLDA-ID
+                     SQLVAR-INDEX
+                     SQL-DATA-TYPE
+                     SQL-HOST-VAR-LENGTH
+            BY REFERENCE DB-USER
+            BY VALUE 0
+                     0
+
+           MOVE 8 TO SQL-HOST-VAR-LENGTH
+           MOVE 452 TO SQL-DATA-TYPE
+           MOVE 2 TO SQLVAR-INDEX
+           MOVE 2 TO SQLDA-ID
+
+           CALL "sqlgstlv" USING 
+            BY VALUE SQLDA-ID
+                     SQLVAR-INDEX
+                     SQL-DATA-TYPE
+                     SQL-HOST-VAR-LENGTH
+            BY REFERENCE DB-PASSWORD
+            BY VALUE 0
+                     0
+
+           MOVE 0 TO SQL-OUTPUT-SQLDA-ID 
+           MOVE 2 TO SQL-INPUT-SQLDA-ID 
+           MOVE 5 TO SQL-SECTIONUMBER 
+           MOVE 29 TO SQL-CALL-TYPE 
+
+           CALL "sqlgcall" USING
+            BY VALUE SQL-CALL-TYPE 
+                     SQL-SECTIONUMBER
+                     SQL-INPUT-SQLDA-ID
+                     SQL-OUTPUT-SQLDA-ID
+                     0
+
+           CALL "sqlgstop" USING
+            BY VALUE 0
+                .
+
+	IF SQLCODE = 0
+		DISPLAY "connection successful"
+	ELSE
+		DISPLAY "connection failed. SQLCODE: " SQLCODE
+	END-IF.
+       stop run.
+       
--- db2integration/main.cob
+++ db2integration/main.cob
@@ -6,8 +6,18 @@
 
        data division.
        working-storage section.
+       EXEC SQL INCLUDE SQLCA END-EXEC.
+       01 db2inst1 PIC X(8) value "db2inst1".
+       01 password PIC x(8) value "password".
+       01 Program-pass-fields.
+          05 Firstnme         Pic x(30).
        01  var-data pic x(20) value "hello world".
        procedure division.
            display var-data
+           EXEC SQL
+            CONNECT TO testdb
+            USER :db2inst1
+            USING :password 
+           END-EXEC.
        stop run.
        
